57ab1155e621e26139f72e246d5dc1abd996a5ec
Chendhur Arasu G <chendhurg@workhall.com>
2024-10-21 22:49:56 +0530

    Changes made during 21/10/24

--- Addition/index.js
+++ Addition/index.js
@@ -1,4 +1,5 @@
 var addition = require('chendhur1515');
 var result  = addition.add(1,3);
+// 
 var result2  = addition.multiply(2,3);
 console.log(result,result2);
--- NPM/index.js
+++ NPM/index.js
@@ -1,3 +1,5 @@
 var _  = require('underscore');
 var res =_.contains([1,2,3],3);
+
+// 
 console.log(res);
