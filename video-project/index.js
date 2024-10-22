417350aa6248a780436c23e7f978a90c0f147556
Chendhur Arasu G <chendhurg@workhall.com>
2024-10-22 23:07:11 +0530

    in folder building restful-services did the crud operations get,put,post,delete

---
+++ video-project/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app   = express();
+
+const movies =[
+    {name:'dark knight', id:1, language:'english'},
+    {name:'man of steel', id:2, language:'english'},
+    {name:'VIP', id:3, language:'tamil'},
+];
+
+
+
+
+
